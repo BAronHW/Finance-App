33f782e400601a4b004ef5e6cb3910ab3890315d
Aaron <aaronwan808@gmail.com>
2024-09-21 23:03:57 +0100

    removed some useless comments

--- front-end/lib/apolloClient.ts
+++ front-end/lib/apolloClient.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 function createApolloClient() {
   return new ApolloClient({
     link: new HttpLink({
-      uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || "http://localhost:4000/graphql", // Your GraphQL server endpoint
+      uri: "http://localhost:4000/graphql",
     }),
     cache: new InMemoryCache(),
   });
