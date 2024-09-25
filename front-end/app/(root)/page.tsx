3cb47cbc0832797c934b401e2ced74aa7dcfb2f9
Aaron <aaronwan808@gmail.com>
2024-09-25 03:09:25 +0100

    adding some new components

--- front-end/app/(root)/page.tsx
+++ front-end/app/(root)/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
       </div>
       <main className="flex-grow flex items-center justify-center">
         <h1 className="text-2xl font-bold">Welcome to Your Finance App</h1>
+        
       </main>
     </div>
   );
