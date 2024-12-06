acce817c4e98d2f8924aab01e76e68bf5e112998
Aaron <aaronwan808@gmail.com>
2024-12-06 01:59:56 +0000

    pushing fixing the fetch user button

--- front-end/src/components/testComp.tsx
+++ front-end/src/components/testComp.tsx
@@ -2,16 +2,25 @@
 
 import { useLazyQuery } from '@apollo/client';
 import React from 'react';
-import { User } from '../lib/GraphQL/User';
 import { Button } from './ui/button';
 import { GET_ALL_USERS } from '@/src/lib/GraphQL/Users';
 
 export default function TestComp() {
-  const [getUsers, { data, loading, error }] = useLazyQuery<{ users: User[] }>(GET_ALL_USERS);
+  const [getUsers, { data, loading, error }] = useLazyQuery<{ users: any[] }>(GET_ALL_USERS);
 
-  const fetchUsers = () => {
-    getUsers();
+  const fetchUsers = async () => {
+    try {
+      console.log('Fetching users...');
+      const result = await getUsers();
+      console.log('Query result:', result);
+    } catch (err) {
+      console.error('Error fetching users:', err);
+    }
   };
+
+  console.log('Current data:', data);
+  console.log('Loading:', loading);
+  console.log('Error:', error);
 
   return (
     <div>
@@ -20,8 +29,8 @@ export default function TestComp() {
       {error && <p>Error: {error.message}</p>}
       {data && data.users && (
         <ul>
-          {data.users.map((user: User) => (
-            <li key={user.id}>{user.firstName} {user.lastName}</li>
+          {data.users.map((user) => (
+            <li key={user.id}>{user.username || `User ${user.id}`}</li>
           ))}
         </ul>
       )}
