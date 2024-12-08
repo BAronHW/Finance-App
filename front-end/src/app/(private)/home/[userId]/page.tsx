d22440b4f2e9b3eec14282e483e5858afa5cde60
Aaron <aaronwan808@gmail.com>
2024-12-08 15:06:16 +0000

    made the header reflect the name of the current user from firebase auth obj

--- front-end/src/app/(private)/home/[userId]/page.tsx
+++ front-end/src/app/(private)/home/[userId]/page.tsx
@@ -5,12 +5,18 @@ import Header from '@/src/components/Header';
 import { Button } from '@/src/components/ui/button';
 import TestComp from '@/src/components/testComp';
 import { BentoDemo } from '@/src/components/BentoDemo';
+import { useParams } from 'next/navigation';
+import { getAuth } from 'firebase/auth';
 
 export default function Home() {
+  const params = useParams();
+  const id = params?.userId;
+  const auth = getAuth();
+  const currentUser = auth.currentUser;
   return (
     <div className="flex flex-col min-h-screen">
       <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <Header name='Aaron' appMoto='Manage your student funds' accBal={10} />
+        <Header name={currentUser?.displayName ?? ""} appMoto='Manage your student funds' accBal={10} />
       </div>
       <main className="flex-grow flex items-center justify-center flex-col gap-10 m-7">
         <BentoDemo />
