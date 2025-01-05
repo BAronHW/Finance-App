741f2d557c5ef6f35fcaf854adb1bd0ebb02bb4e
Aaron <aaronwan808@gmail.com>
2025-01-05 21:16:34 +0000

    starting the plaid integration by adding plaidconfig object

---
+++ backend/api/utils/PlaidConfiguration.ts
@@ -0,0 +1,12 @@
+import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid'
+
+// Initialize Plaid client
+const configuration = new Configuration({
+  basePath: PlaidEnvironments.sandbox,
+  baseOptions: {
+    headers: {
+      'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
+      'PLAID-SECRET': process.env.PLAID_SECRET,
+    },
+  },
+});
