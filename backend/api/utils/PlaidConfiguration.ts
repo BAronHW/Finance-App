e321b463233b84ef8d3aeda23b6aedc15679d5a3
Aaron <aaronwan808@gmail.com>
2025-01-05 22:47:44 +0000

    removign keys

--- backend/api/utils/PlaidConfiguration.ts
+++ backend/api/utils/PlaidConfiguration.ts
@@ -1,12 +1,11 @@
 import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid'
 
-// Initialize Plaid client
-export const configuration = new Configuration({
+const configuration = new Configuration({
   basePath: PlaidEnvironments.sandbox,
   baseOptions: {
     headers: {
-      'PLAID-CLIENT-ID': "674f55e0e0e97c001b192502",
-      'PLAID-SECRET': "7efcea23c44308c0c51873f42e8df1",
+      'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID, // need to fix this so its actually importing the right stuff from dotenv
+      'PLAID-SECRET': process.env.PLAID_SECRET,
     },
   },
 });
