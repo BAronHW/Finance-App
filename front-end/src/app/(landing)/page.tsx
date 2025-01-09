e4d526d504c6d3e1cc0d481bc15c0379fd13f4b9
Peter Li <petercyli02@gmail.com>
2025-01-09 02:39:40 +0000

    partial implementation of new landing page

--- front-end/src/app/(landing)/page.tsx
+++ front-end/src/app/(landing)/page.tsx
@@ -1,25 +1,29 @@
-'use client'
+"use client";
 
-import Link from 'next/link';
-import Header from '@/components/customComponents/Header';
-import { Button } from '@/components/ui/button';
-import TestComp from '@/components/customComponents/testComp';
-import "../globals.css"
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import TestComp from "@/components/customComponents/testComp";
+import "../globals.css";
 
 export default function Landing() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <main className="flex-grow flex items-center justify-center flex-col gap-10 m-7">
-        <div className="mt-6 space-y-4 text-center">
-          <p className="text-lg">Welcome to your dashboard!</p>
+    <div className="min-h-screen">
+      <main className="gap-10 m-7">
+        <div className="absolute right-20 grid grid-cols-2 gap-2">
           <Link href="/sign-in" passHref>
-            <Button>Go to Sign In</Button>
+            <Button size="lg" className="text-xl rounded-full" variant="default">Login</Button>
           </Link>
-          <p className="text-lg">New user?</p>
           <Link href="/sign-up" passHref>
-            <Button>Go to Sign Up</Button>
+            <Button size="lg" className="text-xl rounded-full border-solid border-2 border-slate-400" variant="secondary">Sign Up</Button>
           </Link>
-          <TestComp />
+        </div>
+        <div className="flex flex-row justify-center">
+          <div className="flex flex-col space-y-10">
+            <h1 className="text-9xl font-sans text-center">FinApp</h1>
+            <p className="relative t-100 text-left text-5xl font-mono max-w-3xl">
+              Your personal finance tracker
+            </p>
+          </div>
         </div>
       </main>
     </div>
