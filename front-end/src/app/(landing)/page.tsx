caf1462c6963ca1526c31ca255098a0fe0289373
Peter Li <petercyli02@gmail.com>
2025-01-11 00:34:36 +0000

    added carousel with random images as placeholder landing page

--- front-end/src/app/(landing)/page.tsx
+++ front-end/src/app/(landing)/page.tsx
@@ -2,8 +2,15 @@
 
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import TestComp from "@/components/customComponents/testComp";
 import "../globals.css";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Image from "next/image";
 
 export default function Landing() {
   return (
@@ -11,13 +18,25 @@ export default function Landing() {
       <main className="gap-10 m-7">
         <div className="absolute right-20 grid grid-cols-2 gap-2">
           <Link href="/sign-in" passHref>
-            <Button size="lg" className="text-xl rounded-full" variant="default">Login</Button>
+            <Button
+              size="lg"
+              className="text-xl rounded-full bg-slate-800 hover:bg-slate-400"
+              variant="default"
+            >
+              Login
+            </Button>
           </Link>
           <Link href="/sign-up" passHref>
-            <Button size="lg" className="text-xl rounded-full border-solid border-2 border-slate-400" variant="secondary">Sign Up</Button>
+            <Button
+              size="lg"
+              className="text-xl rounded-full border-solid border-2 border-slate-400 hover:bg-slate-400"
+              variant="secondary"
+            >
+              Sign Up
+            </Button>
           </Link>
         </div>
-        <div className="flex flex-row justify-center">
+        <div className="flex justify-center">
           <div className="flex flex-col space-y-10">
             <h1 className="text-9xl font-sans text-center">FinApp</h1>
             <p className="relative t-100 text-left text-5xl font-mono max-w-3xl">
@@ -25,6 +44,52 @@ export default function Landing() {
             </p>
           </div>
         </div>
+        <Carousel
+          opts={{
+            loop: true,
+          }}
+          className="mt-20"
+        >
+          <CarouselContent>
+            <CarouselItem className="flex justify-center">
+              <Image
+                src="/pexels-bemistermister-380782.jpg"
+                alt="Placeholder 1"
+                height={300}
+                width={800}
+                className="rounded-xl shadow-md hover:shadow-lg"
+              />
+            </CarouselItem>
+            <CarouselItem className="flex justify-center">
+              <Image
+                src="/pexels-mayday-1545743.jpg"
+                alt="Placeholder 2"
+                height={300}
+                width={800}
+                className="rounded-xl shadow-md hover:shadow-lg"
+              />
+            </CarouselItem>
+            <CarouselItem className="flex justify-center">
+              <img
+                src="/pexels-vince-2227774.jpg"
+                alt="Placeholder 3"
+                height={300}
+                width={800}
+                className="rounded-xl shadow-md hover:shadow-lg"
+              />
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious 
+            className="ml-24"
+            variant="outline"
+            size="lg"
+          />
+          <CarouselNext 
+            className="mr-24"
+            variant="outline"
+            size="lg"
+          />
+        </Carousel>
       </main>
     </div>
   );
